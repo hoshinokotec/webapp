6aec867a5dd8ba8ca6d5014f75ba18519ca547c2
{hoshiko.naoki} <{naoki.tennis1023@gmail.com}>
2022-12-08 11:09:59 +0900

    プロジェクト作成

---
+++ dist/app.js
@@ -0,0 +1,175 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+class Projectstatus {
+    constructor() {
+        this.listner = [];
+        this.project = [];
+    }
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        this.instance = new Projectstatus;
+        return this.instance;
+    }
+    addlistener(listnerFn) {
+        this.listner.push(listnerFn);
+    }
+    addProjects(title, descriptor, manday) {
+        const newProject = {
+            id: Math.random().toString(),
+            title: title,
+            descriptor: descriptor,
+            manday: manday,
+        };
+        this.project.push(newProject);
+        for (const listenrFn of this.listner) {
+            listenrFn(this.project.slice());
+        }
+    }
+}
+Projectstatus.instance = new Projectstatus;
+const projectstatus = Projectstatus.getInstance();
+function validate(validatableInput) {
+    let isvalid = true;
+    if (validatableInput.required) {
+        isvalid = isvalid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minlength != null &&
+        typeof validatableInput.value === 'string') {
+        isvalid = isvalid && validatableInput.value.length >= validatableInput.minlength;
+    }
+    if (validatableInput.maxlength != null &&
+        typeof validatableInput.value === 'string') {
+        isvalid = isvalid && validatableInput.value.length <= validatableInput.maxlength;
+    }
+    if (validatableInput.min != null &&
+        typeof validatableInput.value === 'number') {
+        isvalid = isvalid && validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null &&
+        typeof validatableInput.value === 'number') {
+        isvalid = isvalid && validatableInput.value <= validatableInput.max;
+    }
+    return isvalid;
+}
+function autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+}
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        this.assinedProjects = [];
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        projectstatus.addlistener((projects) => {
+            this.assinedProjects = projects;
+            this.renderProjects();
+        });
+        this.attach();
+        this.rendercontent();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+    renderProjects() {
+        const ListEl = document.getElementById(`${this.type}-projects-list`);
+        for (const Prjitem of this.assinedProjects) {
+            const Listitem = document.createElement('li');
+            Listitem.textContent = Prjitem.titile;
+            ListEl.appendChild(Listitem);
+        }
+    }
+    rendercontent() {
+        const listid = `${this.type}-projects-list`;
+        this.element.querySelector('ul').id = listid;
+        this.element.querySelector('h2').textContent =
+            this.type === "active" ? '実行中のプロジェクト' : '完了プロジェクト';
+    }
+}
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.mandayInputElement = this.element.querySelector('#manday');
+        this.configure();
+        this.attach();
+    }
+    getUserIpunt() {
+        const inputtitle = this.titleInputElement.value;
+        const inputdescriptor = this.descriptionInputElement.value;
+        const inputmanday = this.mandayInputElement.value;
+        const titletvalidate = {
+            value: inputtitle,
+            required: true,
+        };
+        const descriptvalidate = {
+            value: inputdescriptor,
+            required: true,
+            minlength: 5,
+        };
+        const mandayvalidate = {
+            value: +inputmanday,
+            required: true,
+            min: 1,
+            max: 1000,
+        };
+        if (!validate(titletvalidate) ||
+            !validate(descriptvalidate) ||
+            !validate(mandayvalidate)) {
+            alert("入力値は正しくありません。再度入力して下さい。");
+            return;
+        }
+        else {
+            return [inputtitle, inputdescriptor, +inputmanday];
+        }
+    }
+    inputclear() {
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.mandayInputElement.value = "";
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        const userinput = this.getUserIpunt();
+        if (Array.isArray(userinput)) {
+            const [titile, desicript, man] = userinput;
+            projectstatus.addProjects(titile, desicript, man);
+            this.inputclear();
+        }
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+__decorate([
+    autobind
+], ProjectInput.prototype, "submitHandler", null);
+const prjInput = new ProjectInput();
+const ac_prjList = new ProjectList('active');
+const ed_prjList = new ProjectList('finished');
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,238 @@
+class Projectstatus {
+  private listner :  any  = [];
+  private project : any [] = [];
+  private static instance = new Projectstatus;
+
+  private constructor (){
+  }
+  static getInstance (){
+    if (this.instance){
+      return this.instance
+    }
+    this.instance = new Projectstatus;
+    return this.instance;
+  }
+
+  addlistener (listnerFn : Function ){
+    this.listner.push(listnerFn);
+  }
+
+
+  addProjects(title : string , descriptor : string , manday : number ){
+    const newProject = {
+      id  : Math.random().toString(),
+      title : title,
+      descriptor : descriptor,
+      manday : manday,
+    }
+    this.project.push(newProject);
+    for(const listenrFn of this.listner ){
+      listenrFn(this.project.slice())
+    }
+  }
+}
+
+const projectstatus  = Projectstatus.getInstance();
+
+// validate interface
+interface validate {
+  value : string | number;
+  required? : boolean;
+  minlength? : number;
+  maxlength? : number;
+  min? : number;
+  max? : number
+}
+
+function validate(validatableInput : validate){
+
+  let isvalid = true;
+  if (validatableInput.required){
+    isvalid = isvalid && validatableInput.value.toString().trim().length !== 0;
+   }
+  if (
+    validatableInput.minlength != null  &&
+    typeof validatableInput.value === 'string'
+  ){
+    isvalid = isvalid && validatableInput.value.length >= validatableInput.minlength; 
+  }
+  if (
+    validatableInput.maxlength != null  &&
+    typeof validatableInput.value === 'string'
+  ){
+    isvalid = isvalid && validatableInput.value.length <= validatableInput.maxlength; 
+  }
+  if (
+    validatableInput.min != null  &&
+    typeof validatableInput.value === 'number'
+  ){
+    isvalid = isvalid && validatableInput.value >= validatableInput.min; 
+  }
+  if (
+    validatableInput.max != null  &&
+    typeof validatableInput.value === 'number'
+  ){
+    isvalid = isvalid && validatableInput.value <= validatableInput.max; 
+  }
+  return isvalid;
+}
+
+
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+//ProjectList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+  assinedProjects : any[] ;
+  constructor(private type : 'active' | 'finished') {
+    this.templateElement = document.getElementById(
+      'project-list',
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    this.assinedProjects = [];
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true,
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`
+    projectstatus.addlistener((projects : any[]) => {
+      this.assinedProjects = projects ;
+      this.renderProjects();
+    })
+    this.attach()
+    this.rendercontent()
+}
+
+private attach() {
+  this.hostElement.insertAdjacentElement('beforeend', this.element);
+
+}
+
+private renderProjects(){
+  const ListEl = document.getElementById( `${this.type}-projects-list`)! as HTMLUListElement
+  for (const Prjitem of this.assinedProjects){
+  const Listitem =document.createElement('li');
+  Listitem.textContent = Prjitem.titile;
+  ListEl.appendChild(Listitem);
+  }
+}
+
+private rendercontent(){
+  const listid = `${this.type}-projects-list`
+  this.element.querySelector('ul')!.id=listid
+  this.element.querySelector('h2')!.textContent=
+   this.type === "active"  ? '実行中のプロジェクト' : '完了プロジェクト';
+ }
+}
+
+// ProjectInput Class
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input',
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true,
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector(
+      '#title',
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description',
+    ) as HTMLInputElement;
+    this.mandayInputElement = this.element.querySelector(
+      '#manday',
+    ) as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private getUserIpunt ():[string ,string ,number] | void {
+  const inputtitle = this.titleInputElement.value;
+  const inputdescriptor = this.descriptionInputElement.value;
+  const inputmanday = this.mandayInputElement.value;
+  const titletvalidate :validate = {
+    value : inputtitle ,
+    required : true ,
+  }
+  const descriptvalidate :validate = {
+    value : inputdescriptor ,
+    required : true ,
+    minlength : 5 ,
+  }
+  const mandayvalidate :validate = {
+    value : +inputmanday ,
+    required : true ,
+    min : 1 ,
+    max : 1000 ,
+  }
+  if (
+    !validate(titletvalidate) ||
+    !validate(descriptvalidate)||
+    !validate(mandayvalidate)
+    ){
+        alert("入力値は正しくありません。再度入力して下さい。")
+        return;
+    }else{
+      return [inputtitle,inputdescriptor,+inputmanday];
+    }
+  }
+
+  private inputclear(){
+    this.titleInputElement.value = ""
+    this.descriptionInputElement.value = ""
+    this.mandayInputElement.value = ""
+  }
+
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userinput =this.getUserIpunt();
+    if (Array.isArray(userinput)){
+      const [titile,desicript ,man] = userinput;
+      projectstatus.addProjects(titile,desicript,man);
+      this.inputclear()
+    }
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
+const ac_prjList = new ProjectList('active');
+const ed_prjList = new ProjectList('finished');
